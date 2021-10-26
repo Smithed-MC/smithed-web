7ea132ae7fb86a332b2da3ebd9ae382501e0ec92
TheNuclearNexus <thenuclearnexus@gmail.com>
2021-10-26 10:58:24 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { palette } from '.';
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+`
+
+const DownloadButton = styled.button`
+  width: 196px;
+  font-size: 2rem;
+  height: 48px;
+  border-radius: 4px;
+`
+
+const detectUserEnviroment: () => 'windows'|'macos'|'ubuntu'|'unknown' = () => {
+  const agent = navigator.userAgent
+  if(agent.indexOf('Win') !== -1)
+    return 'windows';
+  else if(agent.indexOf('Mac') !== -1)
+    return 'macos';
+  else if(agent.indexOf('Linux') !== -1)
+    return 'ubuntu';
+  return 'unknown';
+  
+}
+
+const downloadStable = () => {
+  const os = detectUserEnviroment();
+  const link = 'https://api.github.com/repos/TheNuclearNexus/smithed/releases/latest';
+  if(os === 'unknown') window.open(link)
+  fetch(link).then(async (resp) => {
+    const data = await resp.json()
+    console.log(data)
+    for(let a of data["assets"]) {
+      if(os === 'windows' && a["name"].endsWith('.exe')) {
+        window.open(a["browser_download_url"])
+        break;
+      }
+      else if(os === 'macos' && a["name"].endsWith('.dmg')) {
+        window.open(a["browser_download_url"])
+        break;
+      }
+      if(os === 'ubuntu' && a["name"].endsWith('.AppImage')) {
+        window.open(a["browser_download_url"])
+        break;
+      }
+    }
+  })
+}
+
+const downloadNightly = () => {
+  const os = detectUserEnviroment();
+
+  if(os !== 'unknown')
+    window.open(`https://nightly.link/TheNuclearNexus/smithed/workflows/build-prod/master/${os}-artifacts.zip`)
+  else
+    window.open(`https://nightly.link/TheNuclearNexus/smithed/workflows/build-prod/master`)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div style={{textAlign: 'center', backgroundColor: palette.darkAccent}}>
+        <h1>{'<SMITHED/>'}</h1>
+        <h2>{'{Datapack Launcher}'}</h2>
+        <div style={{backgroundColor: palette.lightAccent, height: 6}}/>
+      </div>
+      <br/>
+      <div style={{display: 'flex', flexDirection:'column', alignItems:'center', width: '100%'}}>
+        <h2 style={{color: palette.text}}>Download</h2>
+        <div style={{display:'flex',gap: 8}}>
+          <DownloadButton onClick={downloadStable}>Stable</DownloadButton>
+          <DownloadButton onClick={downloadNightly} style={{backgroundColor:'#C274FF'}}>Nightly</DownloadButton>
+        </div>
+
+      </div>
+    </AppContainer>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import styled from 'styled-components';
 import reportWebVitals from './reportWebVitals';
+
+export const palette = {
+  darkAccent: '#1B48C4',
+  lightAccent: '#216BEA',
+  darkBackground: '#24232B',
+  lightBackground: '#2F2F38',
+  text: '#FFFFFF',
+  subText: '#A0A0A0',
+  titlebar: '#FFFFFF'
+}
+
+const IndexContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  background-color: ${palette.lightBackground};
+
+  position:absolute;
+  top:0px;
+  right:0px;
+  bottom:0px;
+  left:0px;
+
+  div {
+    font-family: Inconsolata;
+    color: ${palette.text};
+  }
+
+  h1 {
+    font-size: 4rem;
+    margin: 0px;
+    font-family: Disket-Bold;
+  }
+  h2 {
+    font-size: 2.5rem;
+    margin: 0px;
+    font-family: Disket-Bold;
+    color: ${palette.subText};
+  }
+  h3 {
+    font-size: 1.25rem;
+    margin: 0px;
+    font-family: Disket-Bold;
+    color: ${palette.subText};
+  }
+  button {
+    background-color: ${palette.lightAccent};
+
+    border: none;
+    color: ${palette.text};
+    font-family: Disket-Bold;
+
+    :hover {
+      filter: brightness(85%);
+    }
+    :active {
+      filter: brightness(60%);
+    }
+  }
+
+`
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexContainer>
+      <App />
+    </IndexContainer>
   </React.StrictMode>,
   document.getElementById('root')
 );
