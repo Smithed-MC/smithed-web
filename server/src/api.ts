cbdce9808eb4789e7ced4d9748e3e51574c82171
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-07-01 09:12:55 -0400

    **fix**: trying to not break everything

--- server/src/api.ts
+++ server/src/api.ts
@@ -5,7 +5,8 @@ import { backendApp, frontendApp } from "./main.js";
 export function register() {
     backendApp.get('/download', handleDownload)
     backendApp.get('/increment-download', handleIncDownload)
-
+    backendApp.get('/api/download', handleDownload)
+    backendApp.get('/api/increment-download', handleIncDownload)
     // Make sure to wipe the cache every so often to save space on disk
     setInterval(cleanCache, 10 * 1000)
 }
