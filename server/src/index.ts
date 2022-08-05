98a4e6a44376ccd7907db48fe1276b79834d84dc
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-08-04 23:17:21 -0400

    **fix**: should work again on 17

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
 
 
-// import {Blob} from 'blob-polyfill'
-// import Worker from 'web-worker'
+import {Blob} from 'blob-polyfill'
+import Worker from 'web-worker'
 // import Parallel from 'paralleljs'
-// globalThis.Blob = Blob
-// globalThis.Worker = Worker
+globalThis.Blob = Blob
+globalThis.Worker = Worker
 import {configure} from '@zip.js/zip.js'
 
 
 import('./main.js').then((main) => {   
-    configure({useWebWorkers: false})
+    // configure({useWebWorkers: false})
     main.start()
 })
 
